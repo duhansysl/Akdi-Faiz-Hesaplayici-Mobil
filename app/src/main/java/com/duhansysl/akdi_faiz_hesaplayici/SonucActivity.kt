ac6034e9ce695dea185cf0c5f7c4eeaadf32cf4b
duhań <duhan2002@gmail.com>
2024-12-16 20:24:07 +0300

    get the values we transferred with intent and declare them with variables

--- app/src/main/java/com/duhansysl/akdi_faiz_hesaplayici/SonucActivity.kt
+++ app/src/main/java/com/duhansysl/akdi_faiz_hesaplayici/SonucActivity.kt
@@ -17,6 +17,22 @@ class SonucActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        // Veriyi diğer aktiviteden inten sayesinde alma
+        val toplamGun = intent.getFloatExtra("toplamGun", 0.0f)
+        val toplamBorc = intent.getFloatExtra("toplamBorc", 0.0f)
+        val asgariOdemeMiktari = intent.getFloatExtra("asgariOdemeMiktari", 0.0f)
+        val odenenBocMiktari = intent.getFloatExtra("odenenBocMiktari", 0.0f)
+        val kalanBorcMiktari = intent.getFloatExtra("kalanBorcMiktari", 0.0f)
+        val kalanAsgariBorcMiktari = intent.getFloatExtra("kalanAsgariBorcMiktari", 0.0f)
+        val alisverisFaizBirinci = intent.getFloatExtra("alisverisFaizBirinci", 0.0f)
+        val alisverisFaizIkinci = intent.getFloatExtra("alisverisFaizIkinci", 0.0f)
+        val toplamFaizTutari = intent.getFloatExtra("toplamFaizTutari", 0.0f)
+        val vergiTutari = intent.getFloatExtra("vergiTutari", 0.0f)
+        val toplamMaliyet = intent.getFloatExtra("toplamMaliyet", 0.0f)
+
+        // Gelen verileri ViewBinding ile gösterme
+        // binding.textView.text = "Received Float: $receivedFloat\nReceived String: $receivedString"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
